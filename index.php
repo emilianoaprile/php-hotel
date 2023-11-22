f9f74744dd8190f405f98be7601acb4bd2be194e
EmilianoAprile <emilianoaprile77@icloud.com>
2023-11-22 17:16:45 +0100

    ADDED - table from bootstrap

--- index.php
+++ index.php
@@ -40,15 +40,6 @@ $hotels = [
 
 ];
 
-foreach ($hotels as $key => $hotel) {
-    // var_dump($hotel['name']);
-    // var_dump($hotel['description']);
-    // var_dump($hotel['parking']);
-    // var_dump($hotel['vote']);
-    // var_dump($hotel['distance_to_center']);
-    var_dump($hotel);
-}
-
 ?>
 
 <!DOCTYPE html>
@@ -57,38 +48,37 @@ foreach ($hotels as $key => $hotel) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Hotel</title>
 </head>
 
 <body>
 
-    <div>
-        <?php
-        foreach ($hotels as $key => $hotel) {
-            ?>
-            <h1>
-                <?php echo $hotel['name']; ?>
-            </h1>
-
-            <p>descrizione:
-                <?php echo $hotel['description']; ?>
-            </p>
-
-            <p>parcheggio:
-                <?php echo $hotel['parking']; ?>
-            </p>
-
-            <p>voto:
-                <?php echo $hotel['vote']; ?>
-            </p>
-
-            <p>distanza dal centro:
-                <?php echo $hotel['distance_to_center']; ?>
-            </p>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Parking</th>
+                <th scope="col">Vote</th>
+                <th scope="col">Distance To Center</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php  foreach ($hotels as $key => $hotel) { ?>
+            <tr>
+                <th scope="row"><?php echo $hotel['name']; ?></th>
+                <td><?php echo $hotel['description']; ?></td>
+                <td><?php echo $hotel['parking'] ? 'Si' : 'No' ; ?></td>
+                <td><?php echo $hotel['vote']; ?></td>
+                <td><?php echo $hotel['distance_to_center']; ?> km</td>
+            </tr>
             <?php
-        }
-        ?>
-    </div>
+            }
+            ?>
+        </tbody>
+    </table>
 
 
 

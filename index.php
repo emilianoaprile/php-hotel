a80459ae29abce408c161baa20d2c6628afd7643
EmilianoAprile <emilianoaprile77@icloud.com>
2023-11-22 16:04:22 +0100

    ADDED - data printed on page

--- index.php
+++ index.php
@@ -40,8 +40,60 @@ $hotels = [
 
 ];
 
-foreach ($hotels as $key => $value) {
-    var_dump($key, $value);
+foreach ($hotels as $key => $hotel) {
+    // var_dump($hotel['name']);
+    // var_dump($hotel['description']);
+    // var_dump($hotel['parking']);
+    // var_dump($hotel['vote']);
+    // var_dump($hotel['distance_to_center']);
+    var_dump($hotel);
 }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel</title>
+</head>
+
+<body>
+
+    <div>
+        <?php
+        foreach ($hotels as $key => $hotel) {
+            ?>
+            <h1>
+                <?php echo $hotel['name']; ?>
+            </h1>
+
+            <p>descrizione:
+                <?php echo $hotel['description']; ?>
+            </p>
+
+            <p>parcheggio:
+                <?php echo $hotel['parking']; ?>
+            </p>
+
+            <p>voto:
+                <?php echo $hotel['vote']; ?>
+            </p>
+
+            <p>distanza dal centro:
+                <?php echo $hotel['distance_to_center']; ?>
+            </p>
+            <?php
+        }
+        ?>
+    </div>
+
+
+
+</body>
+
+
+
+</html>
